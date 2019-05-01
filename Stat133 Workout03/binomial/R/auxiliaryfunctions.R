bcdf3680825d6c68ab556a62a1fc6c7b67d8f023
SophiaSophiaY <36497664+SophiaSophiaY@users.noreply.github.com>
2019-05-01 14:30:09 -0700

    Update auxiliaryfunctions.R

--- Stat133 Workout03/binomial/R/auxiliaryfunctions.R
+++ Stat133 Workout03/binomial/R/auxiliaryfunctions.R
@@ -1,30 +1,17 @@
 
-# title: aux_mean()
-# description: given the trials and probability, return the mean
-# param: trials
-# param: prob
-# return: mean
-# example: aux_mean(10, 0.3)
+#this function take two arguments: trials and prob, and return the mean
 aux_mean<- function(trials, prob){
   return(trials*prob)
 }
 
-# title: aux_variance()
-# description: given the trials and probability, return the variance
-# param: trials
-# param: prob
-# return: variance
-# example: aux_variance(10, 0.3)
+
+#this function take two arguments: trials and prob, and return the variance
 aux_variance <- function(trials,prob){
   return(trials*prob*(1-prob))
 }
 
-# title: aux_mode()
-# description: given the trials and probability, return the mode
-# param: trials
-# param: prob
-# return: mode
-# example: aux_mode(10, 0.3)
+
+#this function take two arguments: trials and prob, and return the mode
 aux_mode <- function(trials,prob){
   m <- trials * prob + prob
   if (m %% 1 == 0) {
@@ -34,22 +21,14 @@ aux_mode <- function(trials,prob){
   }
 }
 
-# title: aux_skewness()
-# description: given the trials and probability, return the skewness
-# param: trials
-# param: prob
-# return: skewness
-# example: aux_skewness(10, 0.3)
+
+#this function take two arguments: trials and prob, and return the skewness
 aux_skewness <- function(trials,prob){
   return((1-2*prob)/sqrt(trials*prob*(1-prob)))
 }
 
-# title: aux_kurtosis()
-# description: given the trials and probability, return the kurtosis
-# param: trials
-# param: prob
-# return: kurtosis
-# example: aux_kurtosis(10, 0.3)
+
+#this function take two arguments: trials and prob, and return the kurtosis
 aux_kurtosis <- function(trials, prob){
   return((1-6*prob*(1-prob)) / (trials*prob*(1-prob)))
 }
