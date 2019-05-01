80bd48c806b748d23864cf09b1c94c7f0bcb2177
SophiaSophiaY <36497664+SophiaSophiaY@users.noreply.github.com>
2019-05-01 14:35:26 -0700

    Update checkerfunctions.R

--- Stat133 Workout03/binomial/R/checkerfunctions.R
+++ Stat133 Workout03/binomial/R/checkerfunctions.R
@@ -1,10 +1,5 @@
 
-# title: check_prob()
-# description: test if an input prob is a valid probability value
-# param: prob
-# return: TRUE or error
-# example: check_prob(0.2)
-
+#this is a private auxiliary function check_prob() to test if an input prob is a valid probability value (i.e. 0 ≤ p ≤ 1).
 check_prob <- function(prob){
   if (!is.numeric(prob)) {
     stop("invalid prob input")
@@ -17,12 +12,7 @@ check_prob <- function(prob){
 }
 
 
-# title: check_trials()
-# decsription: test if an input trials is a valid value for number of trials
-# param: trials
-# return: TRUE or error
-# example: check_trials(2)
-
+#this is a private auxiliary function check_trials() to test if an input trials is a valid value for number of trials (i.e. n is a non-negative integer).
 check_trials <- function(trials){
   if (is.integer(trials) & trials >= 0) {
     return(TRUE)
@@ -32,13 +22,7 @@ check_trials <- function(trials){
 }
 
 
-# title: check_success()
-# description: test if an input success is a valid value for number of successes
-# param: success
-# param: trials
-# return: TRUE or error
-# example: check_success(c(1,2),5)
-
+#this is a private auxiliary function check_success() to test if an input success is a valid value for number of successes (i.e. 0 ≤ k ≤ n).
 check_success <- function(success, trials){
   if(any(success < 0 | !is.integer(success))){
     stop("invalid success value")
